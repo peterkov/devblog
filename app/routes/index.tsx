7b2a3d8566b6855af10c5deb9a8516fdec1895f2
Peter Kovacs <peteczz@hotmail.com>
2021-12-31 08:35:56 +0100

    wip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,31 @@
+import * as postB from "./posts/initial.mdx";
+import {Link, useLoaderData} from "@remix-run/react";
+
+function postFromModule(mod: any) {
+  return {
+    slug: mod.filename.replace(/\.mdx?$/, ""),
+    ...mod.attributes.meta
+  };
+}
+
+export function loader() {
+  return [
+    postFromModule(postB),
+  ];
+}
+
 export default function Index() {
+  const posts = useLoaderData();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {posts.map((post: any) => (
+            <li key={post.slug}>
+              <Link to={'posts/' + post.slug}>{post.title}</Link>
+              {post.description ? (
+                  <p>{post.description}</p>
+              ) : null}
+            </li>
+        ))}
       </ul>
-    </div>
   );
 }
